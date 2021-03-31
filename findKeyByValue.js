5f24ea8fa220590f4692dcf614b9ecf7962ae96e
Roshan Nasseri <roshan.sep26@gmail.com>
2021-03-31 22:03:39 +0000

    code for finding key values with their values

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(obj, val) {
+  for (let keyName in obj) {
+    if (obj[keyName] === val) {
+      return keyName;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
