86f6d2513286385e937060a27cbea9903eb7b732
Roshan Nasseri <roshan.sep26@gmail.com>
2021-04-01 19:52:02 +0000

    Implement Map

---
+++ map.js
@@ -0,0 +1,29 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+const assertArraysEqual = function(expected, actual) {
+  if (eqArrays(expected, actual)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = (map(words, word => word[0]));
+console.log(assertArraysEqual(results1,results1));
+
